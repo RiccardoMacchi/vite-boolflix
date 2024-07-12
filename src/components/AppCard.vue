ab47edeeee37eff1351186e6034bac98289d4df3
RiccardoMacchi <rick.mac97@outlook.com>
2024-07-12 18:50:53 +0200

    creato layout e effetto rotazione card migliorata apporssimazione stelle per eccesso e aggiunto effetta logica per valutazione con hover aggiunto effetto hover rotazione card

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -24,8 +24,10 @@ export default{
 
 <template>
 <!-- FILM -->
-    <h2>FILMS</h2>
     <div class="container">
+        <h2>FILMS</h2>
+    </div>
+    <div v-if="store.filmList.length > 0" class="container">
         <!-- Console -->
         <span>{{ console.log("medodo in span")}}{{ consoleMeto() }}</span>
         <span>{{ console.log("span",store.filmList) }}</span>
@@ -37,21 +39,30 @@ export default{
             <div class="card_info">
                 <h2>{{film.title}}</h2>
                 <h3>{{film.original_title}}</h3>
-                <img v-if="store.flags.includes(film.original_language)" :src="'../src/assets/' + film.original_language + '.png'" :alt="film.original_language" id="flagimg">
-                <span v-else>{{film.original_language}}</span>
-                <h4>{{ Math.floor(film.vote_average / 2) }}</h4>
-                <div v-for="i in 5" >
-                    <span v-if="i <= Math.floor(film.vote_average / 2)"><i class="fa-solid fa-star"></i></span>
+                <div class="flagimg">
+                    <img v-if="store.flags.includes(film.original_language)" :src="'../src/assets/' + film.original_language + '.png'" :alt="film.original_language">
+                    <span v-else>{{film.original_language}}</span>
+                </div>
+                <h4>Valutazione degli utenti:</h4>
+                <div class="fb_stars" v-for="i in 5" >
+                    <span v-if="i <= Math.round(film.vote_average / 2)"><i class="fa-solid fa-star"></i></span>
                     <span v-else><i class="fa-regular fa-star"></i></span>
                 </div>
+                <div class="card_txt">
+                    <h4>Panoramica</h4>
+                    <p>{{film.overview}}</p>
+                </div>
+                
                 <!-- <span>{{ consoleMeto() }}</span> -->
             </div>
         </div>
     </div>
-
+    <div class="container" v-else><h5>La ricerca non ha prodotto risultati</h5></div>
 <!-- SERIE TV -->
-    <h2>SERIE TV</h2>
     <div class="container">
+        <h2>SERIE TV</h2>
+    </div>
+    <div v-if="store.serieList.length > 0" class="container">
         <div class="wrapper_card" v-for="(serie, i) in store.serieList" :key="i">
             <div class="card_img">
                 <img :src="'https://image.tmdb.org/t/p/w342' + serie.poster_path" :alt="serie.name">
@@ -59,49 +70,97 @@ export default{
             <div class="card_info">
                 <h2>{{serie.name}}</h2>
                 <h3>{{serie.original_name}}</h3>
-                <img v-if="store.flags.includes(serie.original_language)" :src="'../src/assets/' + serie.original_language + '.png'" :alt="serie.original_language" id="flagimg">
-                <span v-else>{{serie.original_language}}</span>
-                <div v-for="i in 5" >
-                    <span v-if="i <= Math.floor(serie.vote_average / 2)"><i class="fa-solid fa-star"></i></span>
+                <div class="flagimg">
+                    <img v-if="store.flags.includes(serie.original_language)" :src="'../src/assets/' + serie.original_language + '.png'" :alt="serie.original_language">
+                    <span v-else>{{serie.original_language}}</span>
+                </div>
+                <h4>Valutazione degli utenti:</h4>
+                <div class="fb_stars" v-for="i in 5" @mouseover="serie.vote_average = i * 2">
+                    <span v-if="i <= Math.round(serie.vote_average / 2)"><i class="fa-solid fa-star"></i></span>
                     <span v-else><i class="fa-regular fa-star"></i></span>
                 </div>
+                <div class="card_txt">
+                    <h4>Panoramica</h4>
+                    <p>{{serie.overview}}</p>
+                </div>
+                
             </div>
         </div>
      </div>
+     <div class="container" v-else><h5>La ricerca non ha prodotto risultati</h5></div>
 <!-- Arrey di lingue se inclusa stampa se no span -->
 </template>
 
 <style lang="scss">
+
 .wrapper_card{
     flex-basis: calc(100% / 4);
     display: flex;
+    text-align: center;
+    position: relative;
+    transform-style: preserve-3d;
+    transition: all 1s ease;
 
-    #flagimg{
+    div{
+        margin: 0 auto;
+    }
+    
+    .flagimg{
         max-width: 115px;
+        display: block;
+    }
+
+    .fb_stars{
+        display: inline-block;
+    }
+    .card_txt{
+        text-align: left;
+        padding: 0 20px;
     }
 
     .card_info{
         background-color: black;
-        display: none;
+        // display: none;
         width: 100%;
+        height: 100%;
+        color: white;
+        overflow-y: auto;
+        position: absolute;
+        backface-visibility: hidden;
+        transform: rotateY(180deg);
+    }
 
-        span,
-        h2,
-        h3,
-        h4 {
-            color: white;
-        }
+    .card_img{
+        // width: 100%;
+        height: 100%;
+        // position: absolute;
+        backface-visibility: hidden;
+    }
+
+    i{
+        color: yellowgreen;
+
     }
 
 }
+h2{
+    color: red;
+    width: 100%;
+    font-size: 1.5rem;
+    margin: 25px 0;
+}
 
-.container div:hover{
-    .card_info{
-        display: block;
-    }
-    .card_img{
-        display: none;
-    }
+.wrapper_card:hover{
+    // .card_info{
+        
+    // 
+        transform: rotateY(180deg);
+    
+}
+
+h5{
+    color: white;
+    font-size: 2rem;
 }
 
 </style>
--- src/components/AppHeaderSearch.vue
+++ src/components/AppHeaderSearch.vue
@@ -14,10 +14,28 @@ export default {
 </script>
 
 <template>
-    <div>
-        <input type="text" v-model="store.newFilmSearch" @keyup.enter="$emit('newSearch')" >
-        <button >Search</button>
-    </div>
+    <header>
+        <div class="container">
+            <h1>BOOLFLIX</h1>
+            <input type="text" v-model="store.newFilmSearch" placeholder="Cerca qui tra milioni di serie e film" @keyup="$emit('newSearch')" >
+        </div>
+    </header>
 </template>
-<style>
+<style lang="scss" scoped>
+header{
+    background-color: black;
+    color: red;
+}
+.container{
+    justify-content: space-between;
+    align-items: center;
+    height: 75px;
+
+    h1{
+        font-size: 2rem;
+    }
+    input{
+        width: 50%;
+    }
+}
 </style>
